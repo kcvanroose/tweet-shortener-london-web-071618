ea59b0f77a922f09ea0c10597eb73bf91f9d21a9
casey van-roose <kcvanroose@gmail.com>
2018-07-15 11:30:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+require 'pry'
+
+def dictionary 
+ substitutes = {
+  "hello" => 'hi',
+  "to" => '2',
+  "two"=> '2',
+  "too"=> '2',
+  "be"=> 'b',
+  "you"=> 'u',
+  "at"=> '@',
+  "and"=> '&'}
+end
+
+
+def word_substituter(tweet)
+  
+  array= tweet.split( )
+  keys = dictionary.key
+ binding.pry
+  array.each do |long_word| 
+    keys.each do |key|
+      if long_word == key 
+        long_word = dictionary[key]
+        array << long_word
+      else
+        array << long_word
+      end
+    end
+  end
+  array.to_s
+end
+
+
